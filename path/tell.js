6783a272fe57a2763ad62c7cbce222f18c29ef07
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2024-12-19 16:58:43 +0530

    adds changeDirectory function

--- path/tell.js
+++ path/tell.js
@@ -1,23 +1,38 @@
-const curDir = "~";
-const promptName = "tell " + curDir + " % ";
+let curDir = ["~"];
+
+const promptName = function () {
+  return "tell " + curDir.at(-1) + " % ";
+};
 
 const echo = function (args) {
   return args.join(" ");
+};
+
+const changeDirectory = function ([curPath]) {
+  curDir = curDir.concat(curPath.split("/"));
 };
 
 const runCommand = function (command, args) {
   switch (command) {
     case "echo":
       return echo(args);
+    case "cd":
+      return changeDirectory(args);
+  }
+};
+
+const displayMessage = function (message) {
+  if (message) {
+    console.log(message);
   }
 };
 
 const runShell = function () {
   while (true) {
-    const commandToRun = prompt(promptName);
+    const commandToRun = prompt(promptName());
     const [command, ...args] = commandToRun.split(" ");
 
-    console.log(runCommand(command, args));
+    displayMessage(runCommand(command, args));
   }
 };
 
